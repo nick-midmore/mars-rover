7d46e9f5a77875f6a1b8fe2ef6280e58d583ebed
Nick Midmore <nmidmore@gmail.com>
2024-12-05 12:04:05 +0000

    add ParsePosition

--- MarsRover/Input/InputParser.cs
+++ MarsRover/Input/InputParser.cs
@@ -6,12 +6,31 @@ public class InputParser
     private Regex rgMovement = new(@"^[LMRlmr]*$");
     public void ParseInput(string input)
     {
-        var inputArr = input.ToCharArray();
+        var inputArr = input.Split(' ');
         if (String.IsNullOrEmpty(input)) Console.WriteLine("Input cannot be empty!");
 
-        else if (rgPlateau.IsMatch(input)) Plateau.InitSize(inputArr[0], inputArr[2]);
-        else if (rgPosition.IsMatch(input)) Plateau.Rovers.Add(new Rover(new Position(1, 1, Bearing.N)));
-        else if (rgMovement.IsMatch(input)) input.ToCharArray();
+        else if (rgPlateau.IsMatch(input)) Plateau.InitSize(Convert.ToInt32(inputArr[0]), Convert.ToInt32(inputArr[2]));
 
+        else if (rgPosition.IsMatch(input)) ParsePosition(input); 
+
+        else if (rgMovement.IsMatch(input)) input.ToCharArray();
+    }
+
+    public Rover ParsePosition(string input)
+    {
+        var inputArray = input.Split(' ');
+        int x = Convert.ToInt32(inputArray[0]);
+        int y = Convert.ToInt32(inputArray[1]); 
+        Bearing b = inputArray[2].ToLower() switch
+        {
+            "n" => Bearing.N,
+            "e" => Bearing.E,
+            "s" => Bearing.S,
+            "w" => Bearing.W
+        };
+
+        Position resultPosition = new(x, y, b);
+        Rover rover = new(resultPosition);
+        return rover;
     }
 }
