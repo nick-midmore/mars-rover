b3e585cfb587a8c5deb800510a46d386f60e6e06
Nick Midmore <nmidmore@gmail.com>
2024-12-05 10:19:34 +0000

    Add project files.

---
+++ MarsRoverTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+ namespace MarsRoverTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
